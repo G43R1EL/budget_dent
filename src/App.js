aeae462f9c5d7602e22c196fabafe42e6341b391
Nelson Gabriel Hernández <gabrielymarg@gmail.com>
2022-05-25 21:51:13 -0300

    Removed wrapper component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Wrapper from './components/Wrapper/Wrapper';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Wrapper />
+      <NavBar />
     </div>
   );
 }
